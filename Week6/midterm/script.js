0485d2f1e1fa4c44764b0a47727323596cb77a5a
sErV1N <masayukiems@sbcglobal.net>
2021-03-30 16:44:51 -0700

    Beutified all code to look cleaner, made javascript for auto-prompting modal
    Code beautified so it's easier to review and read; added code in the javascript file for auto-prompting the modal upon visiting the index page.

--- Week6/midterm/script.js
+++ Week6/midterm/script.js
@@ -1,20 +1,29 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+
+window.addEventListener("load", function () {
+  //let mymodal = document.getElementById("exampleModal");//
+  var myModal = new bootstrap.Modal(document.getElementById('exampleModal'))
+  console.log(myModal);
+  console.log(bootstrap);
+  myModal.show();
+
+})
