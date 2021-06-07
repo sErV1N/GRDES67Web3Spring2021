46a5bb96eb02c786e594a0b6c84eb5895ffd2c1f
sErV1N <masayukiems@sbcglobal.net>
2021-06-07 07:12:13 -0700

    blog set up, checkout page made, faq set up, images added, jquery file prepped all files beautified
    blog set up with corresponding products and images, checkout page created and to be altered; FAQ set up and to be altered; images to go with each jumbotron added, jquery file added for later jquery manipulation for checkout; all files beautified for cleanliness

--- Week14/Final/script.js
+++ Week14/Final/script.js
@@ -1,5 +1,6 @@
 var shoppingCart = function () {
-    var obj = {}, items = [];
+    var obj = {},
+        items = [];
 
     obj.AddItem = function (itemNo, quantity, price) {
         var item = {};
@@ -32,3 +33,34 @@ var shoppingCart = function () {
     return obj;
 };
 var cart = new shoppingCart();
+
+// JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+
+//modal
+  window.addEventListener("load", function () {
+    //let mymodal = document.getElementById("exampleModal");//
+    var myModal = new bootstrap.Modal(document.getElementById('exampleModal'))
+    console.log(myModal);
+    console.log(bootstrap);
+    myModal.show();
+  
+  })
