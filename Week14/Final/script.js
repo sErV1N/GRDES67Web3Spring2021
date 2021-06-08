0227a87e2b48142f1350d8af7aa02ceaf41e17a9
sErV1N <masayukiems@sbcglobal.net>
2021-06-08 02:02:04 -0700

    3 sample product pages established; script updated with additional javascript for adding to cart; style adjusted for adding to cart

--- Week14/Final/script.js
+++ Week14/Final/script.js
@@ -1,3 +1,37 @@
+let btn = document.querySelector('.mainBtn');
+let mbtn = document.querySelector('.minusBtn');
+let pbtn = document.querySelector('.plusBtn');
+
+btn.addEventListener("click",()=>{
+  if(btn.innerText == 'ADD TO CART'){
+
+  }
+  btn.innerText = 1;
+  pbtn.style.display = 'inline-block';
+  mbtn.style.display = 'inline-block';
+})
+
+mbtn.addEventListener("click",()=>{
+  if(btn.innerText == 5){
+    pbtn.style.display = 'inline-block';
+  }
+  if(btn.innerText < 2){
+  btn.innerText = 'ADD TO CART';
+  pbtn.style.display = 'none';
+  mbtn.style.display = 'none';
+}else{
+  btn.innerText = btn.innerText - 1;
+  }
+})
+
+pbtn.addEventListener("click",()=>{
+  btn.innerText = +(btn.innerText) + +1;
+
+  if(btn.innerText == 5){
+    pbtn.style.display = 'none';
+ }
+})
+
 var shoppingCart = function () {
     var obj = {},
         items = [];
